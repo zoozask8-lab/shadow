dcb96de411555110bf19c48d022a430f4e42ce55
sothea <zoozask8@gamil.com>
2026-05-07 22:37:47 +0700

    today.js

--- today.js
+++ today.js
@@ -12,7 +12,7 @@ function alearn(){
             block.style.minHeight = "100px";
             block.style.backgroundColor = "white";
             block.style.border = "solid 2px pink";
-            block.style.display = "block"
+            block.style.display = "block";
             h2.style.color = "pink";
             h2.style.textShadow = "1px 2px 5px black";
             h2.style.fontSize = "40px";
@@ -28,8 +28,8 @@ function alearn(){
             h2.style.textShadow = "1px 2px 5px black";
             h2.style.fontSize = "40px";
             h2.innerHTML = "I don't know you!";
-            btn.style.fontSize = "20px"
-            btn.style.textShadow = " 1px 2px 10px white"
+            btn.style.fontSize = "20px";
+            btn.style.textShadow = " 1px 2px 10px white";
             btn.style.color = "red";
             btn.style.backgroundColor = "black";
             btn.style.boxShadow = " 2px 2px 5px white";
@@ -43,4 +43,17 @@ function alearn(){
 function BTN(){
     let block = document.getElementById("block");
     block.style.display = "none";
+}
+
+function BtnImg(){
+    let vDo = document.getElementById("vDo");
+    let video = document.getElementById("video");
+    vDo.style.display = "block";
+    video.src="https://www.youtube.com/embed/5u4xTa3LR2U?si=kylgXmRFlqxiQoyd";
+    video.style.width = "100%";
+    video.style.height = "500px";
+    video.style.marginTop = "30px"; 
+    video.style.marginLeft = "300px"; 
+    video.style.border = "solid 5px rgb(29, 106, 150)"
+    video.style.aspectRatio = "16/9";
 }
